f8134cb8c749704dfc1f69d8640fc53d130084b6
Dan Wolf <dandwolf@hotmail.com>
2026-07-29 20:05:03 -0500

    Trying a new way to build.

--- docs/server/angular-app-engine-manifest.mjs
+++ docs/server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: '/Author-Website',
+  basePath: '/',
   allowedHosts: [],
   supportedLocales: {
   "en-US": ""
--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -2,38 +2,38 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/Author-Website/',
+  baseHref: '/',
   locale: undefined,
   routes: [
   {
     "renderMode": 2,
-    "route": "/Author-Website/Home"
+    "route": "/Home"
   },
   {
     "renderMode": 2,
-    "route": "/Author-Website/Books"
+    "route": "/Books"
   },
   {
     "renderMode": 2,
-    "route": "/Author-Website/ShortStories"
+    "route": "/ShortStories"
   },
   {
     "renderMode": 2,
-    "route": "/Author-Website/AboutMe"
+    "route": "/AboutMe"
   },
   {
     "renderMode": 2,
-    "route": "/Author-Website/**"
+    "route": "/**"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 24657, hash: '5dcf8e3d311647cedb91eee38f5bc25c7bd3518dc9c779f8bc59bc726298a8e6', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 17173, hash: '170aaa5f794947d8c11411ff75d770861c9138277884115ebce10793a337dc55', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'Books/index.html': {size: 56702, hash: '6a462e22d3f537059dff51e708367e64ac8555fc2835623b988313cd65f4d562', text: () => import('./assets-chunks/Books_index_html.mjs').then(m => m.default)},
-    'AboutMe/index.html': {size: 56711, hash: '33cdbd7d553c2d85151879a0b048f2e7062ed7a1c65c6edb6457fad8062ea9bc', text: () => import('./assets-chunks/AboutMe_index_html.mjs').then(m => m.default)},
-    'ShortStories/index.html': {size: 56726, hash: '55e456c5f21ed5805ead87f692c03569537ae54a9d6741ff53b0f81ed4a210c8', text: () => import('./assets-chunks/ShortStories_index_html.mjs').then(m => m.default)},
-    'Home/index.html': {size: 56699, hash: 'dfecc589b5e0e187bb5396114bcee847643e9ab7e94b1fbee27c0ddbcdb22086', text: () => import('./assets-chunks/Home_index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 24642, hash: 'a1addbb2ba3821dbddea38727920223641dd15ea8aebeee2a9beadbbc8c5e6f2', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 17158, hash: '3be5f5f2fc426adc1d97a85f16a3768b3bb0f5116c355f3b1c44fed989c7be2e', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'Books/index.html': {size: 56687, hash: 'da4cea15622fff64ed51a95ea16441f624c8e237777bb480cd935297b873557f', text: () => import('./assets-chunks/Books_index_html.mjs').then(m => m.default)},
+    'Home/index.html': {size: 56684, hash: '71c99a6d91f94ac9d901c5290ceea8022ead8a7e71de1e9baccf21488073a3ef', text: () => import('./assets-chunks/Home_index_html.mjs').then(m => m.default)},
+    'AboutMe/index.html': {size: 56696, hash: '9cc853bbea628544893a64023c73eba1b6b4d0af8dc235aa9827d572c8ec8730', text: () => import('./assets-chunks/AboutMe_index_html.mjs').then(m => m.default)},
+    'ShortStories/index.html': {size: 56711, hash: 'a49de4d130d6c2f6f23a6d54400651e81c9bbd4d8e36cff033bfbd08c3e121bc', text: () => import('./assets-chunks/ShortStories_index_html.mjs').then(m => m.default)},
     'styles-OPUTW5UJ.css': {size: 8043, hash: 'i68XcmjPijU', text: () => import('./assets-chunks/styles-OPUTW5UJ_css.mjs').then(m => m.default)}
   },
 };
